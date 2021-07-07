9899076c6fc34c9eba906db95716d56185b7888e
Cory Calaway <cory.c.calaway@gmail.com>
2021-07-07 11:25:12 -0600

    react component update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
 
 class Counter extends Component {
+
     state = {
-        count: 0
+        count: 0,
+        imageUrl: 'https://picsum.photos/200'
     };
+
+    styles = {
+      fontSize: 20,
+      fontWeight: 'bold'
+    }
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
-        <button>Increment</button>
+        <span>{"ITEM"}</span>
+        <img src={this.state.imageUrl} alt="" />
+        <br></br>
+        <span style={this.styles}>{this.formatCount()}</span>
+        <Button className="">Increment</Button>
       </React.Fragment>
     );
   }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
+
+
+
+
+
+
 
 export default Counter;
