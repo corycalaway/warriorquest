e1cf064e1da9f5d825332418fa06458f373dd82c
Cory Calaway <cory.c.calaway@gmail.com>
2021-07-10 11:38:03 -0600

    binding event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,33 +2,48 @@ import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
+  };
 
-    state = {
-        count: 0,
-        imageUrl: 'https://picsum.photos/200',
-        tags: ['tag1', 'tag2', 'tag3']
-    };
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
 
-    styles = {
-      fontSize: 20,
-      fontWeight: 'bold'
-    }
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
 
-    
+  handleIncrement = () => {
+    console.log("clicked", this.state.count);
+  }
+
   render() {
-
     return (
       <React.Fragment>
         <span>{"ITEM"}</span>
         <img src={this.state.imageUrl} alt="" />
         <br></br>
         <span style={this.styles}>{this.formatCount()}</span>
-        <Button style={{ fontSize: 15}} className={this.getBadgeClasses()}>Increment</Button>
+
+        <Button
+          onClick={this.handleIncrement}
+          style={{ fontSize: 15 }}
+          className={this.getBadgeClasses()}
+        >
+          Increment
+        </Button>
         <ul>
           {
-          //renders a list of items
+            //renders a list of items
           }
-          { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
         </ul>
       </React.Fragment>
     );
@@ -45,11 +60,5 @@ class Counter extends Component {
     return count === 0 ? "Zero" : count;
   }
 }
-
-
-
-
-
-
 
 export default Counter;
