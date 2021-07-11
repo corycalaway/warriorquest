5784d0f9b8ea6720115d3198fc0a792f1ca9b049
Cory Calaway <cory.c.calaway@gmail.com>
2021-07-11 14:07:22 -0600

    fixed mysql

--- serverMySQL/db/schemas/schema.sql
+++ serverMySQL/db/schemas/schema.sql
@@ -8,23 +8,23 @@ USE warriorquest_db;
 CREATE TABLE basicpower (
     basicpower_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     basicpower_name VARCHAR(30) NOT NULL,
-    basicpower_level INT
+    basicpower_level INT UNSIGNED
 );
 
 CREATE TABLE specialpower (
     specialpower_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     specialpower_name VARCHAR(30) NOT NULL,
-    specialpower_level INT
+    specialpower_level INT UNSIGNED
 );
 
 CREATE TABLE savedusers (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     player_name VARCHAR(30),
-    basicpower_id INT NOT NULL,
+    basicpower_id INT UNSIGNED NOT NULL,
     INDEX idx_basicpower(basicpower_id),
-    CONSTRAINT fk_user_basicpower FOREIGN KEY(basicpower_id) REFERENCES basicpower(basicpower_id) ON UPDATE CASCADE ON DELETE RESTRICT,
-    specialpower_id INT NOT NULL,
+    CONSTRAINT fk_user_basicpower FOREIGN KEY(basicpower_id) REFERENCES basicpower(basicpower_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    specialpower_id INT UNSIGNED NOT NULL,
     INDEX idx_specialpower(specialpower_id),
-    CONSTRAINT fk_user_specialpower FOREIGN KEY(specialpower_id) REFERENCES specialpower(specialpower_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT fk_user_specialpower FOREIGN KEY(specialpower_id) REFERENCES specialpower(specialpower_id) ON UPDATE CASCADE ON DELETE CASCADE
     
 );
