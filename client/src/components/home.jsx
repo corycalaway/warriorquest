43ff7ceb31afac1756a752b6fde2d55a34964a65
Cory Calaway <cory.c.calaway@gmail.com>
2021-07-29 20:16:59 -0600

    created rainbow text

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -11,7 +11,7 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1 className="">Welcome to Warrior Quest!</h1>
+        <h1 className="titleClass">Welcome to Warrior Quest!</h1>
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
           <div className="max-w-md w-full space-y-8">
             <div>
