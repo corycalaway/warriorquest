6d5022472a7197b470209a64e044040dbd911af5
Cory Calaway <cory.c.calaway@gmail.com>
2021-08-03 19:06:28 -0600

    login text '

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -15,13 +15,9 @@ class Home extends Component {
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
           <div className="max-w-md w-full space-y-8">
             <div>
-              <img
-                className="mx-auto h-12 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                alt="Workflow"
-              ></img>
-              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                Sign in to your account
+            
+              <h2 className="mt-6 text-center text-3xl font-extrabold rainbowtext">
+                Login in to your account
               </h2>
               <p className="mt-2 text-center text-sm text-gray-600">
                 Or{" "}
@@ -29,7 +25,7 @@ class Home extends Component {
                   href="#"
                   className="font-medium text-indigo-600 hover:text-indigo-500"
                 >
-                  start your 14-day free trial
+                  Create New Account
                 </a>
               </p>
             </div>
